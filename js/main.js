7de4ed7c5e91abb8f45a4a321c727dea66dcb3b1
Yukiji Nakatsuka <lunarbase@outlook.com>
2018-09-11 22:00:35 -0700

    toggle button

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var toggleShow = document.querySelector('.toggle-show')
+var toggleBtn = document.getElementById('menu')
+
+toggleBtn.addEventListener('click', function() {
+  toggleShow.style.display = 'block'
+})
